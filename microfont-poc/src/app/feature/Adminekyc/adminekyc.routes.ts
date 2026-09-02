748ac74d8063e6a045ee4e5d4662727fb78112eb
Sizan <bsse1531@iit.du.ac.bd>
2026-09-02 11:47:28 +0600

    route added to ignore microfont internal

--- microfont-poc/src/app/feature/Adminekyc/adminekyc.routes.ts
+++ microfont-poc/src/app/feature/Adminekyc/adminekyc.routes.ts
@@ -2,6 +2,15 @@ import { Routes } from '@angular/router';
 import { AdminekycShell } from './adminekyc-shell/adminekyc-shell';
 
 export const adminekycRoutes: Routes = [
+
+  //To run Admin panel drectly- new
+  {
+    path: '',
+    redirectTo: 'onboardingAdmineKYC',
+    pathMatch: 'full'
+  },
+
+  //Old Admin panel route
   {
     path: 'onboardingAdmineKYC',
     component: AdminekycShell
